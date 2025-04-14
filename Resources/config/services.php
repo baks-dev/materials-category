9324728b8be2a99c7a30a534be110dc972fa27ce
kingstar <admin@baks.dev>
2025-04-14 20:10:08 +0300

    Baks Development

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -40,6 +40,7 @@ return static function(ContainerConfigurator $container) {
         ->exclude([
             $PATH.'{Entity,Resources,Type}',
             $PATH.'**'.DIRECTORY_SEPARATOR.'*Message.php',
+            $PATH.'**'.DIRECTORY_SEPARATOR.'*Result.php',
             $PATH.'**'.DIRECTORY_SEPARATOR.'*DTO.php',
             $PATH.'**'.DIRECTORY_SEPARATOR.'*Test.php',
         ]);
